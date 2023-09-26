649ae5a65ca5ff33766f183f3c6d65050cc38ecd
Eva-Unit-02 <93512609+Eva-Unit-02@users.noreply.github.com>
2023-09-25 17:21:16 -0700

    Clean up
    Implement common repo, created motor class for all types

--- src/main/java/frc/team3128/subsystems/TestBenchPiston.java
+++ src/main/java/frc/team3128/subsystems/TestBenchPiston.java
@@ -1,9 +1,7 @@
 package frc.team3128.subsystems;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
-import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
